4028eb51361b110feced5e7653078034b11b51af
Jorb Dehmel <jdehmel@outlook.com>
2024-03-07 13:10:19 -0700

    Updated files for 4+1 assignment, finishes the physical view.

--- stratego/network.py
+++ stratego/network.py
@@ -1,6 +1,7 @@
 '''
 Network operations for OOP Stratego. Should send the game state
-from GUI to GUI via pickle.
+from GUI to GUI via pickle. This could take the form of a
+singleton API handler wrapper class.
 '''
 
 import pickle
